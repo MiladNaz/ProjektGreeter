4033fed847b4bff9685f74db9b37c4b0ecb4926f
JoLiJava <73590210+JoLiJava@users.noreply.github.com>
2021-03-30 14:39:09 +0200

    Changed name of test method to greetingAPerson and added a greet method to Greeter.java

--- src/main/java/greeter/Greeter.java
+++ src/main/java/greeter/Greeter.java
@@ -1,6 +1,7 @@
 package greeter;
 
 public class Greeter {
-
-    
+    public String greet(String name) {
+        return "Hello " + name;
+    }
 }
--- src/test/java/greeter/GreeterTest.java
+++ src/test/java/greeter/GreeterTest.java
@@ -3,13 +3,17 @@ package greeter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GreeterTest {
 
     @Test
     @DisplayName("Greeting a person")
-    void greetTest() {
+    void greetingAPerson() {
         Greeter greeter = new Greeter();
 
+        var actual = greeter.greet("Johan");
 
+        assertEquals("Hello Johan", actual);
     }
 }
